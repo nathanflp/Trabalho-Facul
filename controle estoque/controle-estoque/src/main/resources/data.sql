d115baa3728f77c7c6f7960898abc1b6b0c49a53
Nathan Feliphe <nathanfcrocha@gmail.com>
2024-06-03 10:27:00 -0300

    Insert initial values into database

---
+++ controle estoque/controle-estoque/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO produto (nome, marca, quantidade) VALUES
+  ('Martelo', 'Amanco', '45'),
+  ('Cano 20mm', 'Tigre', '1200'),
+  ('30 Metros Cabo De Aço Revestido Pvc Cristal 3/16', 'Deck', '50'),
+  ('Areia Média 20kg ', 'Arckom', '200'),
+  ('Bloco de Concreto Vedação 14X19X39cm ', 'Spar', '5000'),
+  ('Chuveiro Elétrico Eletrônico 127V (110V) 5500w Preto e Cromado Quadrado', 'Acqua Duo Ultra Lorenzetti', '10'),
+  ('Betoneira 400L Prime 220V ', 'Menegotti', '5'),
+  ('Guincho Elétrico Monofásico Tec 500/1000 kg 127V (110V) ', 'CSM', '15'),
+  ('Chave de Impacto', 'Wesco Brushless', '20'),
+  ('Parafusadeira', 'Dexter ', '50')
+  ;
